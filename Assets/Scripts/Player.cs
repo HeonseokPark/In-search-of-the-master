dfbf90e99ddc4843eb9a89e164ea12cc9f2fe003
rhtjdwns <64015904+rhtjdwns@users.noreply.github.com>
2020-04-26 18:45:04 +0900

    Pause System
    일시정지 시스템 - 피드백 있으시면 주시면 감사하겠습니다.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.8f;
+    public float scoreNum = 0;
+    public Text score;
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(0, 0, Input.GetAxis("Vertical"));
+
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(0, -speed, 0);
+        else if (Input.GetKey(KeyCode.D))
+            transform.Rotate(0, speed, 0);
+
+        score.text = $"{scoreNum}";
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin"))
+        {
+            other.gameObject.SetActive(false);
+            scoreNum += 1;
+        }
+    }
+}
