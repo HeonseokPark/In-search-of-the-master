b7b35e5b1f1b233b5ac29302467568944511c0ba
rhtjdwns <64015904+rhtjdwns@users.noreply.github.com>
2020-04-26 19:38:49 +0900

    PauseSystem_02
    수정판

---
+++ PauseSystem/Assets/Scripts/IncreaseCoin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseCoin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameManager.Instance.GetCoin();
+            Destroy(gameObject, 1.5f);
+        }
+    }
+}
