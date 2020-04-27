3cb09b851615e104bc71982bd3d874513ad424fc
rhtjdwns <64015904+rhtjdwns@users.noreply.github.com>
2020-04-28 01:04:13 +0900

    Items
    피버아이템은 피버시스템이 구현이 안되있기에 보류,  피드백 환영

---
+++ Item/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform LootAt;
+    public Vector3 offset = new Vector3(0, 11.36248f, -18.50483f);
+    
+    private void Start()
+    {
+        transform.position = LootAt.position + offset;
+    }
+    private void LateUpdate()
+    {
+        Vector3 DesiredPosition = LootAt.position + offset;
+        DesiredPosition.x = 0;
+        transform.position = Vector3.Lerp(transform.position, DesiredPosition, Time.deltaTime);
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
